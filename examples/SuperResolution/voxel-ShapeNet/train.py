4cd8f69a2ff8620d1e3061a4d95186b9d0ca4fe7
JF <jlafleche@nvidia.com>
2019-11-28 10:29:08 -0500

    Remove MSE option as it does not work

--- examples/SuperResolution/voxel-ShapeNet/train.py
+++ examples/SuperResolution/voxel-ShapeNet/train.py
@@ -18,7 +18,7 @@ import kaolin as kal
 parser = argparse.ArgumentParser()
 parser.add_argument('--shapenet-root', type=str, required=True, help='Root directory of the ShapeNet dataset.')
 parser.add_argument('--cache-dir', type=str, default='cache', help='Directory where intermediate representations will be stored.')
-parser.add_argument('--loss-type', type=str, choices=['MSE', 'NLLL'], default='MSE', help='Specify the loss type to use.')
+parser.add_argument('--expid', type=str, default='superres', help='Unique experiment identifier.')
 parser.add_argument('--device', type=str, default='cuda', help='Device to use')
 parser.add_argument('--categories', type=str, nargs='+', default=['chair'], help='list of object classes to use.')
 parser.add_argument('--epochs', type=int, default=30, help='Number of train epochs.')
@@ -27,8 +27,8 @@ parser.add_argument('-lr', '--learning-rate', type=float, default=1e-3, help='Le
 parser.add_argument('--val-every', type=int, default=5, help='Validation frequency (epochs).')
 parser.add_argument('--print-every', type=int, default=20, help='Print frequency (batches).')
 parser.add_argument('--logdir', type=str, default='log', help='Directory to log data to.')
-parser.add_argument('--save-model', action='store_true', help='Saves the model and a snapshot \
-    of the optimizer state.')
+parser.add_argument('--resume', choices=['none', 'best', 'recent'], default='none',
+                    help='Choose which weights to resume training from (none to start from random initialization.)')
 args = parser.parse_args()
 
 
@@ -47,43 +47,41 @@ dataloader_val = DataLoader(valid_set, batch_size=args.batchsize, shuffle=False,
 
 
 # Model settings
-if args.loss_type == 'MSE':
-    model = EncoderDecoder_32_128().to(args.device)
-    loss_fn = torch.nn.MSELoss()
-elif args.loss_type == 'NLLL':
-    model = EncoderDecoderForNLL_32_128().to(args.device)
-    class_weights = torch.tensor([0.0283, 1 - 0.0283], dtype=torch.float, device=device)
-    loss_fn = torch.nn.NLLLoss(weight=class_weights)
-else:
-    ValueError('Loss Type {0} is not supported.'.format(args.loss_type))
+model = EncoderDecoderForNLL_32_128().to(args.device)
+class_weights = torch.tensor([0.0283, 1 - 0.0283], dtype=torch.float, device=device)
+loss_fn = torch.nn.NLLLoss(weight=class_weights)
 
 optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
 
 
 # Create log directory, if it doesn't already exist
-args.logdir = os.path.join(args.logdir, args.loss_type)
-if not os.path.isdir(args.logdir):
-    os.makedirs(args.logdir)
-    print('Created dir:', args.logdir)
+logdir = os.path.join(args.logdir, args.expid)
+if not os.path.isdir(logdir):
+    os.makedirs(logdir)
+    print('Created dir:', logdir)
 
 # Log all commandline args
-with open(os.path.join(args.logdir, 'args.txt'), 'w') as f:
+with open(os.path.join(logdir, 'args.txt'), 'w') as f:
     json.dump(args.__dict__, f, indent=2)
 
 
 class Engine(object):
     """Engine that runs training and inference.
     Args
-        cur_epoch (int): Current epoch.
         print_every (int): How frequently (# batches) to print loss.
-        validate_every (int): How frequently (# epochs) to run validation.
+        resume_name (str): Prefix of weights from which to resume training. If 'none',
+            no weights are loaded.
     """
 
-    def __init__(self, cur_epoch=0, print_every=1, validate_every=1):
-        self.cur_epoch = cur_epoch
+    def __init__(self, print_every=1, resume_name='none'):
+        self.cur_epoch = 0
         self.train_loss = []
         self.val_loss = []
         self.bestval = 0
+        self.print_every = print_every
+
+        if resume_name != 'none':
+            self.load(resume_name)
 
     def train(self):
         loss_epoch = 0.
@@ -102,13 +100,11 @@ class Engine(object):
             pred = model(inp.unsqueeze(1))
 
             # losses
-            if args.loss_type == 'NLLL':
-                tgt = tgt.long()
+            tgt = tgt.long()
             loss = loss_fn(pred, tgt)
             loss.backward()
             loss_epoch += float(loss.item())
-            if args.loss_type == 'NLLL':
-                pred = pred[:, 1, :, :]
+            pred = pred[:, 1, :, :]
             iou = kal.metrics.voxel.iou(pred.contiguous(), tgt)
 
             # logging
@@ -141,13 +137,11 @@ class Engine(object):
                 pred = model(inp.unsqueeze(1))
 
                 # losses
-                if args.loss_type == 'NLLL':
-                    tgt = tgt.long()
+                tgt = tgt.long()
                 loss = loss_fn(pred, tgt)
                 loss_epoch += float(loss.item())
 
-                if args.loss_type == 'NLLL':
-                    pred = pred[:, 1, :, :]
+                pred = pred[:, 1, :, :]
                 iou = kal.metrics.voxel.iou(pred.contiguous(), tgt)
                 iou_epoch += iou
 
@@ -169,8 +163,22 @@ class Engine(object):
             loss_epoch = loss_epoch / num_batches
             self.val_loss.append(out_iou)
 
-    def save(self):
+    def load(self, resume_name):
+        model_path = os.path.join(logdir, f'{resume_name}.pth')
+        optim_path = os.path.join(logdir, f'{resume_name}_optim.pth')
+        assert os.path.exists(model_path), f'Model weights not found at "{model_path}"'
+        assert os.path.exists(optim_path), f'Optim weights not found at "{optim_path}"'
+        model.load_state_dict(torch.load(model_path))
+        optimizer.load_state_dict(torch.load(optim_path))
 
+        # Read data corresponding to the loaded model
+        with open(os.path.join(logdir, f'{resume_name}.log'), 'r') as f:
+            run_data = json.load(f)
+        self.cur_epoch = run_data['epoch']
+
+        tqdm.write(f'====== Loaded {resume_name} model ======>')
+
+    def save(self):
         save_best = False
         if self.val_loss[-1] >= self.bestval:
             self.bestval = self.val_loss[-1]
@@ -187,24 +195,24 @@ class Engine(object):
         }
 
         # Save the recent model/optimizer states
-        torch.save(model.state_dict(), os.path.join(args.logdir, 'recent.pth'))
-        torch.save(optimizer.state_dict(), os.path.join(args.logdir, 'recent_optim.pth'))
+        torch.save(model.state_dict(), os.path.join(logdir, 'recent.pth'))
+        torch.save(optimizer.state_dict(), os.path.join(logdir, 'recent_optim.pth'))
         # Log other data corresponding to the recent model
-        with open(os.path.join(args.logdir, 'recent.log'), 'w') as f:
+        with open(os.path.join(logdir, 'recent.log'), 'w') as f:
             f.write(json.dumps(log_table))
 
         tqdm.write('====== Saved recent model ======>')
 
         if save_best:
-            torch.save(model.state_dict(), os.path.join(args.logdir, 'best.pth'))
-            torch.save(optimizer.state_dict(), os.path.join(args.logdir, 'best_optim.pth'))
+            torch.save(model.state_dict(), os.path.join(logdir, 'best.pth'))
+            torch.save(optimizer.state_dict(), os.path.join(logdir, 'best_optim.pth'))
             # Log other data corresponding to the recent model
-            with open(os.path.join(args.logdir, 'best.log'), 'w') as f:
+            with open(os.path.join(logdir, 'best.log'), 'w') as f:
                 f.write(json.dumps(log_table))
             tqdm.write('====== Overwrote best model ======>')
 
 
-trainer = Engine()
+trainer = Engine(print_every=args.print_every, resume_name=args.resume)
 
 for epoch in range(args.epochs):
     trainer.train()
