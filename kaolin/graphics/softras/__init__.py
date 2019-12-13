b10f22d148fc167d0c1fd08e1726b7ac6d83aa57
Krishna Murthy <krrish94@gmail.com>
2019-12-13 02:27:20 -0500

    Port softras

--- kaolin/graphics/softras/__init__.py
+++ kaolin/graphics/softras/__init__.py
@@ -26,9 +26,6 @@ from . import functional
 from .mesh import Mesh
 from .renderer import Renderer, SoftRenderer
 from .transform import Projection, LookAt, Look, Transform
-from .lighting import AmbientLighting, DirectionalLighting, Lighting
+# from .lighting import AmbientLighting, DirectionalLighting, Lighting
 from .rasterizer import SoftRasterizer
 from .losses import LaplacianLoss, FlattenLoss
-
-
-__version__ = '1.0.0'
