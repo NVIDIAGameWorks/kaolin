467b4ecd85768fbddc07d8066ef901dfd7bda290
Krishna Murthy <krrish94@gmail.com>
2020-04-27 12:50:21 -0400

    Fix test for examples (use &&)
    Signed-off-by: Krishna Murthy <krrish94@gmail.com>

--- tests/examples/run/test_classification.sh
+++ tests/examples/run/test_classification.sh
@@ -2,13 +2,13 @@
 python ${KAOLIN_HOME}/examples/Classification/pointcloud_classification.py \
     --modelnet-root /data/ModelNet10/ \
     --categories desk dresser \
-    --epochs 1 --transforms-device cuda
-
+    --epochs 1 --transforms-device cuda \
+&& \
 python ${KAOLIN_HOME}/examples/Classification/pointcloud_classification.py \
     --modelnet-root /data/ModelNet10/ \
     --categories desk dresser \
-    --epochs 1 --transforms-device cpu
-
+    --epochs 1 --transforms-device cpu \
+&& \
 python ${KAOLIN_HOME}/examples/Classification/mesh_classification.py \
     --shrec-root /data/SHREC16/ \
     --categories ants cat \
