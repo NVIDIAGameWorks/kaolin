7b4f0b467f23860e6f4ef3ed5992860719dec7e4
Jason Gorski <jasongorski@users.noreply.github.com>
2020-04-29 13:14:11 -0400

    reintroduce nuscenes (#232)
    - Revert "remove nuscenes temporarily (#207)" (94291d9)
    - update copyright
    - git ignore tests/visualize/results/
    
    Signed-off-by: Jason Gorski <jasongorski@users.noreply.github.com>

--- kaolin/datasets/__init__.py
+++ kaolin/datasets/__init__.py
@@ -2,3 +2,8 @@ from .shapenet import *
 from .modelnet import *
 from .shrec import *
 from .scannet import *
+try:
+    from .nusc import NuscDetection
+except ImportError as err:
+    import_err = err
+    print("Warning: unable to import datasets/nusc:\n   %s" % import_err)
