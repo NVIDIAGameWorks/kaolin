33d6d531d61535e8fe17888a2c86430d2f1ce025
Krishna Murthy <krrish94@gmail.com>
2020-05-01 00:06:14 -0400

    Add CI test for examples
    Signed-off-by: Krishna Murthy <krrish94@gmail.com>

---
+++ tests/examples/run/test_softras_examples.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python ${KAOLIN_HOME}/examples/renderers/softras/softras_simple_render.py \
+    --stride 360 \
+    --no-viz
+&& \
+python ${KAOLIN_HOME}/examples/renderers/softras/softras_vertex_optimization.py \
+    --iters 2\
+    --no-viz
+&& \
+python ${KAOLIN_HOME}/examples/renderers/softras/softras_texture_optimization.py \
+    --iters 2 \
+    --no-viz
