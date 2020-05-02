1779d24de2bcc58c731dbc118957de16775b09e4
Krishna Murthy <krrish94@gmail.com>
2020-05-02 10:03:08 -0400

    Fix typo
    Signed-off-by: Krishna Murthy <krrish94@gmail.com>

--- tests/examples/run/test_softras_examples.sh
+++ tests/examples/run/test_softras_examples.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 python ${KAOLIN_HOME}/examples/renderers/softras/softras_simple_render.py \
     --stride 360 \
-    --no-viz
+    --no-viz \
 && \
 python ${KAOLIN_HOME}/examples/renderers/softras/softras_vertex_optimization.py \
     --iters 2\
-    --no-viz
+    --no-viz \
 && \
 python ${KAOLIN_HOME}/examples/renderers/softras/softras_texture_optimization.py \
     --iters 2 \
