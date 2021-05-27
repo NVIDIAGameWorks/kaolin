1c0037bff352f5cb639d8d18d0f1aae46647389b
Clement Fuji Tsang <cfujitsang@nvidia.com>
2021-05-27 21:05:25 +0000

    fine-tune ShapeNet to support both V1 and V2, details the supported synsets
    Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

--- kaolin/io/shapenet.py
+++ kaolin/io/shapenet.py
@@ -19,60 +19,81 @@ from kaolin.io.dataset import KaolinDataset
 from kaolin.io.obj import import_mesh, ignore_error_handler
 
 # Synset to Label mapping (for ShapeNet core classes)
-synset_to_label = {
-    '04379243': 'table',
-    '03211117': 'monitor',
-    '04401088': 'phone',
-    '04530566': 'watercraft',
-    '03001627': 'chair',
-    '03636649': 'lamp',
-    '03691459': 'speaker',
-    '02828884': 'bench',
-    '02691156': 'plane',
-    '02808440': 'bathtub',
-    '02871439': 'bookcase',
-    '02773838': 'bag',
-    '02801938': 'basket',
-    '02880940': 'bowl',
-    '02924116': 'bus',
-    '02933112': 'cabinet',
-    '02942699': 'camera',
-    '02958343': 'car',
-    '03207941': 'dishwasher',
-    '03337140': 'file',
-    '03624134': 'knife',
-    '03642806': 'laptop',
-    '03710193': 'mailbox',
-    '03761084': 'microwave',
-    '03928116': 'piano',
-    '03938244': 'pillow',
-    '03948459': 'pistol',
-    '04004475': 'printer',
-    '04099429': 'rocket',
-    '04256520': 'sofa',
-    '04554684': 'washer',
-    '04090263': 'rifle',
-    '02946921': 'can'
+synset_to_labels = {
+    '04379243': ['table'],
+    '03211117': ['display', 'video display'],
+    '04401088': ['telephone', 'phone', 'telephone set'],
+    '04530566': ['vessel', 'watercraft'],
+    '03001627': ['chair'],
+    '03636649': ['lamp'],
+    '03691459': ['loudspeaker', 'speaker', 'speaker unit', 'loudspeaker system', 'speaker system'],
+    '02828884': ['bench'],
+    '02691156': ['airplane', 'aeroplane', 'plane'],
+    '02808440': ['bathtub', 'bathing tub', 'bath', 'tub'],
+    '02871439': ['bookshelf'],
+    '02773838': ['bag', 'traveling bag', 'travelling bag', 'grip', 'suitcase'],
+    '02801938': ['basket', 'handbasket'],
+    '02880940': ['bowl'],
+    '02924116': ['bus', 'autobus', 'coach', 'charabanc', 'double-decker', 'jitney',
+                 'motorbus', 'motorcoach', 'omnibus', 'passenger vehi'],
+    '02933112': ['cabinet'],
+    '02942699': ['camera', 'photographic camera'],
+    '02958343': ['car', 'auto', 'automobile', 'machine', 'motorcar'],
+    '03207941': ['dishwasher', 'dish washer', 'dishwashing machine'],
+    '03337140': ['file', 'file cabinet', 'filing cabinet'],
+    '03624134': ['knife'],
+    '03642806': ['laptop', 'laptop computer'],
+    '03710193': ['mailbox', 'letter box'],
+    '03761084': ['microwave', 'microwave oven'],
+    '03928116': ['piano', 'pianoforte', 'forte-piano'],
+    '03938244': ['pillow'],
+    '03948459': ['pistol', 'handgun', 'side arm', 'shooting iron'],
+    '04004475': ['printer', 'printing machine'],
+    '04099429': ['rocket', 'projectile'],
+    '04256520': ['sofa', 'couch', 'lounge'],
+    '04554684': ['washer', 'automatic washer', 'washing machine'],
+    '04090263': ['rifle'],
+    '02946921': ['can', 'tin', 'tin can'],
+    '04330267': ['stove'],
+    '02843684': ['birdhouse'],
+    '03513137': ['helmet'],
+    '02992529': ['cellular telephone', 'cellular phone', 'cellphone', 'cell', 'mobile phone'],
+    '03991062': ['pot', 'flowerpot'],
+    '04074963': ['remote control', 'remote'],
+    '03790512': ['motorcycle', 'bike'],
+    '04225987': ['skateboard'],
+    '03593526': ['jar'],
+    '02954340': ['cap'],
+    '03467517': ['guitar'],
+    '04460130': ['tower'],
+    '03759954': ['microphone', 'mike'],
+    '03325088': ['faucet', 'spigot'],
+    '03797390': ['mug'],
+    '03046257': ['clock'],
+    '02747177': ['ashcan', 'trash can', 'garbage can', 'wastebin', 'ash bin', 'ash-bin',
+                 'ashbin', 'dustbin', 'trash barrel', 'trash bin'],
+    '02818832': ['bed'],
+    '03085013': ['computer keyboard', 'keypad'],
+    '02876657': ['bottle'],
+    '04468005': ['train', 'railroad train'],
+    '03261776': ['earphone', 'earpiece', 'headphone', 'phone'],
+    '02834778': ['bicycle', 'bike', 'wheel', 'cycle'],
+    '02858304': ['boat']
 }
 
 # Label to Synset mapping (for ShapeNet core classes)
-label_to_synset = {v: k for k, v in synset_to_label.items()}
-
+label_to_synset = {label: synset for synset, labels in synset_to_labels.items() for label in labels}
 
 def _convert_categories(categories):
-    if categories is None:
-        synset = [value for key, value in label_to_synset.items()]
-    else:
-        if not (c in synset_to_label.keys() + label_to_synset.keys()
-                for c in categories):
-            warnings.warn('Some or all of the categories requested are not part of \
-                ShapeNetCore. Data loading may fail if these categories are not avaliable.')
-        synsets = [label_to_synset[c] if c in label_to_synset.keys()
-                   else c for c in categories]
+    if not (c in synset_to_label.keys() + label_to_synset.keys()
+            for c in categories):
+        warnings.warn('Some or all of the categories requested are not part of \
+            ShapeNetCore. Data loading may fail if these categories are not avaliable.')
+    synsets = [label_to_synset[c] if c in label_to_synset.keys()
+               else c for c in categories]
     return synsets
 
-
-class ShapeNet(KaolinDataset):
+class ShapeNetV1(KaolinDataset):
     r"""ShapeNetV1 Dataset class for meshes.
 
     The `__getitem__` method will return a `KaolinDatasetItem`, with its `data`
@@ -82,7 +103,7 @@ class ShapeNet(KaolinDataset):
         root (str): path to ShapeNet root directory
         categories (list): List of categories to load from ShapeNet. This list may
                            contain synset ids, class label names (for ShapeNetCore classes),
-                           or a combination of both.
+                           or a combination of both. Default: all supported categories.
         train (bool):
             If True, return the training set, otherwise the test set.
             Default: True.
@@ -91,19 +112,84 @@ class ShapeNet(KaolinDataset):
             Default: 0.7
         with_materials (bool): If True, load and return materials. Default: True.
     """
-    def __init__(self, root: str, categories: list, train: bool = True,
+    SUPPORTED_SYNSETS = {
+        '04330267',
+        '02843684',
+        '02871439',
+        '03513137',
+        '02880940',
+        '03001627',
+        '03761084',
+        '02992529',
+        '03991062',
+        '04074963',
+        '03790512',
+        '04225987',
+        '02924116',
+        '04379243',
+        '03593526',
+        '02828884',
+        '03938244',
+        '02954340',
+        '03467517',
+        '02834778',
+        '04099429',
+        '04460130',
+        '03759954',
+        '02858304',
+        '03691459',
+        '04256520',
+        '04530566',
+        '04004475',
+        '02691156',
+        '03211117',
+        '03948459',
+        '03636649',
+        '03325088',
+        '02946921',
+        '02958343',
+        '03797390',
+        '02808440',
+        '03046257',
+        '02747177',
+        '03928116',
+        '02818832',
+        '03337140',
+        '04090263',
+        '04401088',
+        '03207941',
+        '03624134',
+        '02773838',
+        '03085013',
+        '03642806',
+        '03710193',
+        '02933112',
+        '02876657',
+        '04468005',
+        '02801938',
+        '03261776',
+        '04554684',
+        '02942699'
+    }
+
+    def __init__(self, root: str, categories: list = None, train: bool = True,
                  split: float = .7, with_materials=True):
 
         self.root = Path(root)
         self.paths = []
         self.synset_idxs = []
-        self.synsets = _convert_categories(categories)
-        self.labels = [synset_to_label[s] for s in self.synsets]
+        if categories is None:
+            self.synsets = list(self.SUPPORTED_SYNSETS)
+        else:
+            self.synsets = _convert_categories(categories)
+            for s in self.synsets:
+                assert s in self.SUPPORTED_SYNSETS, \
+                    f"{s} is not supported in ShapeNetV1"
+        self.labels = [synset_to_labels[s] for s in self.synsets]
         self.with_materials = with_materials
 
         # loops through desired classes
-        for i in range(len(self.synsets)):
-            syn = self.synsets[i]
+        for i, syn in enumerate(self.synsets):
             class_target = self.root / syn
             if not class_target.exists():
                 raise ValueError(
@@ -137,7 +223,147 @@ class ShapeNet(KaolinDataset):
             'name': self.names[index],
             'path': self.paths[index] / 'model.obj',
             'synset': self.synsets[synset_idx],
-            'label': self.labels[synset_idx]
+            'labels': self.labels[synset_idx]
+        }
+        return attributes
+
+    def get_cache_key(self, index):
+        return self.names[index]
+
+class ShapeNetV2(KaolinDataset):
+    r"""ShapeNetV2 Dataset class for meshes.
+
+    The `__getitem__` method will return a `KaolinDatasetItem`, with its `data`
+    field containing a `kaolin.io.obj.ObjMesh`.
+
+    Args:
+        root (str): path to ShapeNet root directory
+        categories (list): List of categories to load from ShapeNet. This list may
+                           contain synset ids, class label names (for ShapeNetCore classes),
+                           or a combination of both. Default: all supported categories.
+        train (bool):
+            If True, return the training set, otherwise the test set.
+            Default: True.
+        split (float):
+            fraction of the dataset to be used for training (>=0 and <=1).
+            Default: 0.7
+        with_materials (bool): If True, load and return materials. Default: True.
+    """
+
+    SUPPORTED_SYNSETS = {
+        '04330267',
+        '02843684',
+        '02871439',
+        '03513137',
+        '02880940',
+        '03001627',
+        '03761084',
+        '02992529',
+        '03991062',
+        '04074963',
+        '03790512',
+        '04225987',
+        '02924116',
+        '04379243',
+        '03593526',
+        '02828884',
+        '03938244',
+        '02954340',
+        '03467517',
+        '04099429',
+        '04460130',
+        '03759954',
+        '03691459',
+        '04256520',
+        '04530566',
+        '04004475',
+        '02691156',
+        '03211117',
+        '03948459',
+        '03636649',
+        '03325088',
+        '02946921',
+        '02958343',
+        '03797390',
+        '02808440',
+        '03046257',
+        '02747177',
+        '03928116',
+        '02818832',
+        '03337140',
+        '04090263',
+        '04401088',
+        '03207941',
+        '03624134',
+        '02773838',
+        '03085013',
+        '03642806',
+        '03710193',
+        '02933112',
+        '02876657',
+        '04468005',
+        '02801938',
+        '03261776',
+        '04554684',
+        '02942699'
+    }
+
+    def __init__(self, root: str, categories: list = None, train: bool = True,
+                 split: float = .7, with_materials=True):
+
+        self.root = Path(root)
+        self.paths = []
+        self.synset_idxs = []
+        if categories is None:
+            self.synsets = list(self.SUPPORTED_SYNSETS)
+        else:
+            self.synsets = _convert_categories(categories)
+            for s in self.synsets:
+                assert s in self.SUPPORTED_SYNSETS, \
+                    f"{s} is not supported in ShapeNetV2"
+        self.labels = [synset_to_labels[s] for s in self.synsets]
+        self.with_materials = with_materials
+
+        # loops through desired classes
+        for i, syn in enumerate(self.synsets):
+            class_target = self.root / syn
+            if not class_target.exists():
+                raise ValueError(
+                    'Class {0} ({1}) was not found at location {2}.'.format(
+                        syn, self.labels[i], str(class_target)))
+
+            # find all objects in the class
+            if syn == '02958343':  # this class have empty model folders
+                models = sorted([el.parent.parent
+                                 for el in class_target.glob('*/models/model_normalized.obj')])
+            else:
+                models = sorted(class_target.glob('*'))
+            stop = int(len(models) * split)
+            if train:
+                models = models[:stop]
+            else:
+                models = models[stop:]
+            self.paths += models
+            self.synset_idxs += [i] * len(models)
+
+        self.names = [p.name for p in self.paths]
+
+    def __len__(self):
+        return len(self.paths)
+
+    def get_data(self, index):
+        obj_location = self.paths[index] / 'models/model_normalized.obj'
+        mesh = import_mesh(str(obj_location), with_materials=self.with_materials,
+                           error_handler=ignore_error_handler)
+        return mesh
+
+    def get_attributes(self, index):
+        synset_idx = self.synset_idxs[index]
+        attributes = {
+            'name': self.names[index],
+            'path': self.paths[index] / 'models/model_normalized.obj',
+            'synset': self.synsets[synset_idx],
+            'labels': self.labels[synset_idx]
         }
         return attributes
 
--- tests/python/kaolin/io/test_shapenet.py
+++ tests/python/kaolin/io/test_shapenet.py
@@ -17,11 +17,13 @@ import os
 
 import pytest
 import torch
+import random
 
 from kaolin.io.obj import return_type
-from kaolin.io.shapenet import ShapeNet
+from kaolin.io import shapenet
 
-SHAPENET_PATH = '/data/ShapeNet'
+SHAPENETV1_PATH = '/data/ShapeNetCore.v1'
+SHAPENETV2_PATH = '/data/ShapeNetCore.v2'
 SHAPENET_TEST_CATEGORY_SYNSETS = ['02691156']
 SHAPENET_TEST_CATEGORY_LABELS = ['plane']
 SHAPENET_TEST_CATEGORY_SYNSETS_2 = ['02958343']
@@ -30,6 +32,7 @@ SHAPENET_TEST_CATEGORY_SYNSETS_MULTI = ['02691156', '02958343']
 SHAPENET_TEST_CATEGORY_LABELS_MULTI = ['plane', 'car']
 
 ALL_CATEGORIES = [
+    None,
     SHAPENET_TEST_CATEGORY_SYNSETS,
     SHAPENET_TEST_CATEGORY_LABELS,
     SHAPENET_TEST_CATEGORY_SYNSETS_2,
@@ -40,25 +43,34 @@ ALL_CATEGORIES = [
 
 # Skip test in a CI environment
 @pytest.mark.skipif(os.getenv('CI') == 'true', reason="CI does not have dataset")
+@pytest.mark.parametrize('version', ['v1', 'v2'])
 @pytest.mark.parametrize('categories', ALL_CATEGORIES)
 @pytest.mark.parametrize('train', [True, False])
 @pytest.mark.parametrize('with_materials', [True, False])
 class TestShapeNet(object):
 
     @pytest.fixture(autouse=True)
-    def shapenet_dataset(self, categories, train, with_materials):
-        return ShapeNet(root=SHAPENET_PATH,
-                        categories=categories,
-                        train=train,
-                        split=0.7,
-                        with_materials=with_materials)
+    def shapenet_dataset(self, version, categories, train, with_materials):
+        if version == 'v1':
+            return shapenet.ShapeNetV1(root=SHAPENETV1_PATH,
+                                       categories=categories,
+                                       train=train,
+                                       split=0.7,
+                                       with_materials=with_materials)
+        elif version == 'v2':
+            return shapenet.ShapeNetV2(root=SHAPENETV2_PATH,
+                                       categories=categories,
+                                       train=train,
+                                       split=0.7,
+                                       with_materials=with_materials)
+        else:
+            raise ValueError(f"version {version} not recognized")
 
-    @pytest.mark.parametrize('index', [0, -1])
+    @pytest.mark.parametrize('index', [0, -1, None, None])
     def test_basic_getitem(self, shapenet_dataset, index, with_materials):
+        if index is None:
+            index = random.randint(0, len(shapenet_dataset) - 1)
         assert len(shapenet_dataset) > 0
-
-        if index == -1:
-            index = len(shapenet_dataset) - 1
 
         item = shapenet_dataset[index]
         data = item.data
@@ -99,4 +111,36 @@ class TestShapeNet(object):
         assert isinstance(attributes['name'], str)
         assert isinstance(attributes['path'], Path)
         assert isinstance(attributes['synset'], str)
-        assert isinstance(attributes['label'], str)
+        assert isinstance(attributes['labels'], list)
+
+    @pytest.mark.parametrize('index', [-1, -2])
+    def test_neg_index(self, shapenet_dataset, index, with_materials):
+
+        assert len(shapenet_dataset) > 0
+
+        gt_item = shapenet_dataset[len(shapenet_dataset) + index]
+        gt_data = gt_item.data
+        gt_attributes = gt_item.attributes
+
+        item = shapenet_dataset[index]
+        data = item.data
+        attributes = item.attributes
+
+        assert torch.equal(data.vertices, gt_data.vertices)
+        assert torch.equal(data.faces, gt_data.faces)
+
+        if with_materials:
+            assert torch.equal(data.uvs, gt_data.uvs)
+            assert torch.equal(data.face_uvs_idx, gt_data.face_uvs_idx)
+            assert torch.equal(data.materials_order, gt_data.materials_order)
+            for m, gt_m in zip(data.materials, gt_data.materials):
+                assert m.keys() == gt_m.keys()
+                for k in m.keys():
+                    if k == 'name':
+                        assert m[k] == gt_m[k]
+                    else:
+                        assert torch.equal(m[k], gt_m[k])
+        assert attributes['name'] == gt_attributes['name']
+        assert attributes['path'] == gt_attributes['path']
+        assert attributes['synset'] == gt_attributes['synset']
+        assert attributes['labels'] == gt_attributes['labels']
