4446ab6e21e52f3ae4e364c14065642eaad63fe0
Caenorst <cfujitsang@nvidia.com>
2021-12-17 17:11:39 -0500

    Unbatched PCL to SPC - docstring (#498)
    Signed-off-by: operel <operel@nvidia.com>
    
    Co-authored-by: operel <operel@nvidia.com>
    Signed-off-by: Alex Zook <azook@nvidia.com>

--- kaolin/ops/conversions/pointcloud.py
+++ kaolin/ops/conversions/pointcloud.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 
 import torch
+from kaolin.ops.spc.points import quantize_points, points_to_morton, morton_to_points, unbatched_points_to_octree
+from kaolin.rep.spc import Spc
 
-__all__ = ['pointclouds_to_voxelgrids']
+__all__ = ['pointclouds_to_voxelgrids', 'unbatched_pointcloud_to_spc']
 
 def _base_points_to_voxelgrids(points, resolution, return_sparse=False):
     r"""Converts points to voxelgrids. This is the base function for both trianglemeshes_to_voxelgrids
@@ -132,3 +134,59 @@ def pointclouds_to_voxelgrids(pointclouds, resolution, origin=None, scale=None, 
     vg = _base_points_to_voxelgrids(pointclouds, resolution, return_sparse=return_sparse)
 
     return vg
+
+def unbatched_pointcloud_to_spc(pointcloud, level, features=None):
+    r"""This function takes as input a single point-cloud - a set of continuous coordinates in 3D,
+    and coverts it into a :ref:`Structured Point Cloud (SPC)<spc>`, a compressed octree representation where
+    the point cloud coordinates are quantized to integer coordinates.
+
+    Point coordinates are expected to be normalized to the range [-1, 1].
+    If a point is out of the range [-1, 1] it will be clipped to it.
+
+    If ``features`` are specified, the current implementation will average features
+    of points that inhabit the same quantized bucket.
+
+    Args:
+        pointclouds (torch.Tensor):
+            An unbatched pointcloud with shape :math:`(\text{num_points}, 3)`.
+            Coordinates are expected to be normalized to the range [-1, 1].
+        level (int):
+            Maximum number of levels to use in octree hierarchy.
+        features (torch.Tensor, optional):
+            Feature vector containing information per point
+            :math:`(\text{num_points}, \text{feat_dim})`.
+
+    Returns:
+        (kaolin.rep.Spc):
+        A Structured Point Cloud (SPC) object, holding a single-item batch.
+    """
+    points = quantize_points(pointcloud.contiguous(), level)
+
+    # Avoid duplications if cells occupy more than one point
+    unique, unique_keys, unique_counts = torch.unique(points.contiguous(), dim=0,
+                                                      return_inverse=True, return_counts=True)
+
+    # Create octree hierarchy
+    morton, keys = torch.sort(points_to_morton(unique.contiguous()).contiguous())
+    points = morton_to_points(morton.contiguous())
+    octree = unbatched_points_to_octree(points, level, sorted=True)
+
+    # Organize features for octree leaf nodes
+    feat = None
+    if features is not None:
+        # Feature collision of multiple points sharing the same cell is consolidated here.
+        # Assumes mean averaging
+        feat_dtype = features.dtype
+        is_fp = features.is_floating_point()
+
+        # Promote to double precision dtype to avoid rounding errors
+        feat = torch.zeros(unique.shape[0], features.shape[1], device=features.device).double()
+        feat = feat.index_add_(0, unique_keys, features.double()) / unique_counts[..., None].double()
+        if not is_fp:
+            feat = torch.round(feat)
+        feat = feat.to(feat_dtype)
+        feat = feat[keys]
+
+    # A full SPC requires octree hierarchy + auxilary data structures
+    lengths = torch.tensor([len(octree)], dtype=torch.int32)   # Single entry batch
+    return Spc(octrees=octree, lengths=lengths, features=feat)
