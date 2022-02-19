4c1697ae4b80ef5f93b32966527c2956b66aefbc
Alex Zook <azook@nvidia.com>
2022-02-18 16:09:07 -0800

    animate fits on 'test' data
    Signed-off-by: Alex Zook <azook@nvidia.com>

--- examples/tutorial/bbox_kaolin.py
+++ examples/tutorial/bbox_kaolin.py
@@ -12,10 +12,10 @@ import os
 import glob
 
 # hyperparameters
-batch_size = 2
+batch_size_hyper = 2
 mask_weight = 1.0
 lr = 5e-2
-num_epoch = 50
+num_epoch = 300
 
 # load image(s) with camera extrinsics
 rendered_path = "../samples/rendered_clock/"
@@ -24,7 +24,7 @@ train_data = []
 for i in range(num_views):
     data = kaolin.io.render.import_synthetic_view(rendered_path, i, rgb=True, semantic=True)
     train_data.append(data)
-dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, pin_memory=True)
+dataloader = DataLoader(train_data, batch_size=batch_size_hyper, shuffle=True, pin_memory=True)
 
 # set up mesh to optimize
 class DiffBBox:
@@ -41,7 +41,7 @@ class DiffBBox:
         self.vertices.requires_grad = True
         self.faces = self.mesh.faces.cuda()
 
-def project_to_2d(bbox: DiffBBox, image_shape: Tuple[int, int], camera_transform, camera_projection) -> torch.Tensor:
+def project_to_2d(bbox: DiffBBox, batch_size: int, image_shape: Tuple[int, int], camera_transform, camera_projection) -> torch.Tensor:
     face_vertices_camera, face_vertices_image, face_normals = kaolin.render.mesh.prepare_vertices(
                 bbox.vertices.repeat(batch_size, 1, 1),
                 bbox.faces, camera_projection,camera_transform=camera_transform
@@ -65,11 +65,34 @@ def draw_image(gt_mask, pred_mask):
 
     return canvas*255
 
+import matplotlib.animation as animation
+
+def show_renders(dataset: torch.Tensor):
+    with torch.no_grad():
+        batch_size = len(dataset)
+        images = []
+        for idx, data in enumerate(dataset):
+            gt_mask = data['semantic'].cuda()
+            camera_transform = data['metadata']['cam_transform'].cuda()
+            camera_projection = data['metadata']['cam_proj'].cuda()
+            
+            # project model mesh onto 2d image
+            img_shape = (gt_mask.shape[0], gt_mask.shape[1])
+            soft_mask = project_to_2d(bbox, batch_size=1, image_shape=img_shape, camera_transform=camera_transform, camera_projection=camera_projection)
+            image = draw_image(gt_mask.squeeze(), soft_mask)
+            images.append(image)
+
+        # fig, ax = plt.subplots(1, batch_size, figsize=(4, 6))
+        # for i in range(batch_size):
+        #     ax[i].imshow(images[i])
+        
+        return images
 
 # set up model & optimization
 bbox = DiffBBox('../samples/bbox.obj')
 optim = torch.optim.Adam(params=[bbox.vertices], lr=lr)
 
+image_list = []
 for epoch in range(num_epoch):
     for idx, data in enumerate(dataloader):
         optim.zero_grad()
@@ -81,7 +104,7 @@ for epoch in range(num_epoch):
         
         # project model mesh onto 2d image
         img_shape = (gt_mask.shape[1], gt_mask.shape[2])
-        soft_mask = project_to_2d(bbox, image_shape=img_shape, camera_transform=camera_transform, camera_projection=camera_projection)
+        soft_mask = project_to_2d(bbox, batch_size=batch_size_hyper, image_shape=img_shape, camera_transform=camera_transform, camera_projection=camera_projection)
 
         # compute overlap of projection with ground truth segmentation mask
         mask_loss: torch.Tensor = kaolin.metrics.render.mask_iou(soft_mask, gt_mask.squeeze(-1))
@@ -92,10 +115,25 @@ for epoch in range(num_epoch):
         optim.step()
 
     print(f"{epoch} loss: {loss}")
-    for idx in range(batch_size):
-        img = draw_image(gt_mask.squeeze()[idx,:], soft_mask[idx,:])
-    cv2.imshow(f"image", img)
-    
-    # KEYBOARD INTERACTIONS
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+    test_batch_ids = [2, 5, 10]
+    test_viz = [train_data[idx] for idx in test_batch_ids]
+    image_list.append(show_renders(test_viz))
+
+
+# generate final plot
+num_subplots = len(test_batch_ids)
+f, ax = plt.subplots(ncols=num_subplots)
+ims = []
+for i in range(len(image_list)):
+    sp_ims = []
+    for j in range(num_subplots):
+        im = ax[j].imshow(image_list[i][j], animated=True)
+        sp_ims.append(im)
+    # show first to not have blinking animation
+    if i == 0:
+        for j in range(num_subplots):
+            ax[j].imshow(image_list[i][j])
+    ims.append(sp_ims)
+ani = animation.ArtistAnimation(f, ims, interval=30, blit=True, repeat_delay=200)
+ani.save("animation.gif")
+# plt.show()
