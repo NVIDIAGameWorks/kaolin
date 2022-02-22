c91561dee40057573bdc50c7a2adb5fd5ff5f1df
Alex Zook <azook@nvidia.com>
2022-02-22 14:53:00 -0800

    fit rotation. add lr scheduler to fine-tune
    Signed-off-by: Alex Zook <azook@nvidia.com>

--- examples/tutorial/bbox_kaolin.py
+++ examples/tutorial/bbox_kaolin.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from PIL import Image
+from pyrr import Quaternion, matrix44
 from torch.utils.data import DataLoader
 
 import kaolin
@@ -17,8 +18,11 @@ batch_size_hyper = 2
 mask_weight_hyper = 1.0
 mask_occupancy_hyper = 0.05
 mask_overlap_hyper = 1.0
-lr = 5e-3
-num_epoch = 3
+lr = 5e-2
+scheduler_step_size = 5
+scheduler_gamma = 0.5
+num_epoch = 30
+# TODO: stepsize scheduler
 
 # load image(s) with camera extrinsics
 rendered_path = "../samples/rendered_clock/"
@@ -29,17 +33,42 @@ for i in range(num_views):
     train_data.append(data)
 dataloader = DataLoader(train_data, batch_size=batch_size_hyper, shuffle=True, pin_memory=True)
 
+
+# modified from pyrr to use torch Tensor
+def _euler_to_matrix33(pitch, roll, yaw, dtype=torch.float) -> torch.Tensor:
+
+    sP = torch.sin(pitch)
+    cP = torch.cos(pitch)
+    sR = torch.sin(roll)
+    cR = torch.cos(roll)
+    sY = torch.sin(yaw)
+    cY = torch.cos(yaw)
+
+    m1 = torch.stack([cY * cP, -cY * sP * cR + sY * sR, cY * sP * sR + sY * cR])
+    m2 = torch.stack([sP, cP * cR, -cP * sR])
+    m3 = torch.stack([-sY * cP, sY * sP * cR + cY * sR, -sY * sP * sR + cY * cR])
+    mat33 = torch.stack([m1, m2, m3]).cuda()
+    return mat33
+
+
 # set up mesh to optimize
-class DiffBBox:
+class DiffBBox(torch.nn.Module):
     """Represents a 3d bounding box to fit."""
 
     def __init__(self, mesh_file: str):
+        super().__init__()
+
         self._mesh = kaolin.io.obj.import_mesh(mesh_file, with_materials=True)
         self._vertices = None
         self._faces = None
         self._centers = torch.zeros((3,), dtype=torch.float, device="cuda", requires_grad=True)
         self._scales = torch.ones((3,), dtype=torch.float, device="cuda", requires_grad=True)
-        # self._rotations = torch.ones((3,3,), dtype=torch.float, device="cuda", requires_grad=True)
+        # rotations as euler angles
+        # self._rotations = torch.zeros((3,), dtype=torch.float, device="cuda", requires_grad=True)
+        euler = np.random.uniform(0.0, 1.0, size=[3])
+        self._rot_pitch = torch.nn.Parameter(torch.tensor(euler[0], dtype=torch.float))
+        self._rot_roll = torch.nn.Parameter(torch.tensor(euler[1], dtype=torch.float))
+        self._rot_yaw = torch.nn.Parameter(torch.tensor(euler[2], dtype=torch.float))
         self._preprocess()
         # TODO: add pose as quaternion. rotate verts
 
@@ -48,11 +77,19 @@ class DiffBBox:
         self._vertices.requires_grad = False
         self._faces = self._mesh.faces.cuda()
 
+    def _get_transform(self):
+        rot = None  # as 4x4
+        scale = None  # as 4x4
+        translation = None  # as 4x4
+        return torch.matmul(translation, torch.matmul(rot, scale))
+
     @property
     def vertices(self):
         # rotate, scale in xyz, translate
-        # return (torch.matmul(self._vertices, self._rotations) * self._scales) + self._centers
-        return (self._vertices * self._scales) + self._centers
+        # euler = np.random.uniform(0.0, 1.0, size=[3])
+        # rot = _euler_to_matrix33(euler)
+        rot = _euler_to_matrix33(self._rot_pitch, self._rot_roll, self._rot_yaw)
+        return (torch.matmul(self._vertices, rot) * self._scales) + self._centers
 
     @property
     def faces(self):
@@ -142,7 +179,8 @@ def show_renders(dataset: torch.Tensor):
 
 # set up model & optimization
 bbox = DiffBBox("../samples/bbox.obj")
-optim = torch.optim.Adam(params=[bbox._centers, bbox._scales], lr=lr)
+optim = torch.optim.Adam(params=[bbox._centers, bbox._scales, *bbox.parameters()], lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=scheduler_step_size, gamma=scheduler_gamma)
 
 
 image_list = []
@@ -178,14 +216,16 @@ for epoch in range(num_epoch):
         loss.backward()
         optim.step()
 
-        test_batch_ids = [2, 5, 10]
-        test_viz = [train_data[idx] for idx in test_batch_ids]
-        image_list.append(show_renders(test_viz))
+        if idx % 10 == 0:
+            test_batch_ids = [2, 5, 10]
+            test_viz = [train_data[idx] for idx in test_batch_ids]
+            image_list.append(show_renders(test_viz))
 
+            cv2.imshow("test", image_list[-1][0])
+            k = cv2.waitKey(1)
+
+    scheduler.step()
     print(f"{epoch} loss: {loss}")
-    # test_batch_ids = [2, 5, 10]
-    # test_viz = [train_data[idx] for idx in test_batch_ids]
-    # image_list.append(show_renders(test_viz))
 
 
 # generate final plot
