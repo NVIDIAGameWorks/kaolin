2d46c9302861a0b7ff02f855050d525025aee957
Alex Zook <azook@nvidia.com>
2022-03-24 12:24:52 -0700

    cleanup
    Signed-off-by: Alex Zook <azook@nvidia.com>

--- examples/tutorial/bbox_kaolin.py
+++ examples/tutorial/bbox_kaolin.py
@@ -1,87 +1,71 @@
 import glob
 import os
+from pathlib import Path
 from typing import Tuple
 
 import cv2
 import matplotlib.animation as animation
 import numpy as np
 import torch
-import torch.nn.functional as F
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 
 import kaolin
-from examples.tutorial.conversions import (
-    _pad_mat33_to_mat44,
-    euler_to_matrix33,
-    euler_to_matrix44,
-    euler_to_quaternion,
-    matrix33_to_quaternion,
-    quaternion_to_matrix33,
-    quaternion_to_matrix44,
-    quaternion_to_matrix44_v2,
-    scale_to_matrix44,
-    translation_to_matrix44,
-)
-
-# hyperparameters
-batch_size_hyper = 2
-mask_weight_hyper = 1.0
-mask_occupancy_hyper = 0.05
-mask_overlap_hyper = 1.0
-lr = 5e-2
-scheduler_step_size = 5
-scheduler_gamma = 0.5
-num_epoch = 30
-# TODO: stepsize scheduler
-
-# load image(s) with camera extrinsics
-rendered_path = "../samples/rendered_clock/"
-num_views = len(glob.glob(os.path.join(rendered_path, "*_rgb.png")))
-train_data = []
-for i in range(num_views):
-    data = kaolin.io.render.import_synthetic_view(rendered_path, i, rgb=True, semantic=True)
-    train_data.append(data)
-dataloader = DataLoader(train_data, batch_size=batch_size_hyper, shuffle=True, pin_memory=True)
 
 
-# modified from pyrr to use torch Tensor
-def _euler_to_matrix33(pitch, roll, yaw, dtype=torch.float) -> torch.Tensor:
+def vector_normalize(vec: torch.Tensor) -> torch.Tensor:
+    return vec / vec.norm(p=2, dim=-1, keepdim=True)
 
-    sP = torch.sin(pitch)
-    cP = torch.cos(pitch)
-    sR = torch.sin(roll)
-    cR = torch.cos(roll)
-    sY = torch.sin(yaw)
-    cY = torch.cos(yaw)
 
-    m1 = torch.stack([cY * cP, -cY * sP * cR + sY * sR, cY * sP * sR + sY * cR])
-    m2 = torch.stack([sP, cP * cR, -cP * sR])
-    m3 = torch.stack([-sY * cP, sY * sP * cR + cY * sR, -sY * sP * sR + cY * cR])
-    mat33 = torch.stack([m1, m2, m3]).cuda()
+def quaternion_to_matrix33(quat: torch.Tensor) -> torch.Tensor:
+    q = vector_normalize(quat)
+
+    # http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/index.htm
+    qx, qy, qz, qw = q[0], q[1], q[2], q[3]
+    sqw = qw ** 2
+    sqx = qx ** 2
+    sqy = qy ** 2
+    sqz = qz ** 2
+    qxy = qx * qy
+    qzw = qz * qw
+    qxz = qx * qz
+    qyw = qy * qw
+    qyz = qy * qz
+    qxw = qx * qw
+
+    invs = 1 / (sqx + sqy + sqz + sqw)
+    m00 = (sqx - sqy - sqz + sqw) * invs
+    m11 = (-sqx + sqy - sqz + sqw) * invs
+    m22 = (-sqx - sqy + sqz + sqw) * invs
+    m10 = 2.0 * (qxy + qzw) * invs
+    m01 = 2.0 * (qxy - qzw) * invs
+    m20 = 2.0 * (qxz - qyw) * invs
+    m02 = 2.0 * (qxz + qyw) * invs
+    m21 = 2.0 * (qyz + qxw) * invs
+    m12 = 2.0 * (qyz - qxw) * invs
+    r0 = torch.stack([m00, m01, m02])
+    r1 = torch.stack([m10, m11, m12])
+    r2 = torch.stack([m20, m21, m22])
+    mat33 = torch.stack([r0, r1, r2]).T
     return mat33
 
 
-def euler_to_quat(heading, attitude, bank):
-    c1 = torch.cos(heading / 2)
-    c2 = torch.cos(attitude / 2)
-    c3 = torch.cos(bank / 2)
-    s1 = torch.sin(heading / 2)
-    s2 = torch.sin(attitude / 2)
-    s3 = torch.sin(bank / 2)
-    w = c1 * c2 * c3 - s1 * s2 * s3
-    x = s1 * s2 * c3 + c1 * c2 * s3
-    y = s1 * c2 * c3 + c1 * s2 * s3
-    z = c1 * s2 * c3 - s1 * c2 * s3
-    return torch.stack([w, x, y, z])
+class DifferentiableBBox(torch.nn.Module):
+    """Represents a differentiable 3d bounding box.
 
+    Box is paramterized in terms of a fixed mesh and a learned (optimized)
+    transformation in terms of rotation, scaling, and translation.
+    """
 
-# set up mesh to optimize
-class DiffBBox(torch.nn.Module):
-    """Represents a 3d bounding box to fit."""
+    def __init__(self, mesh_file: Path):
+        """Construct a 3d bounding from a mesh filepath.
 
-    def __init__(self, mesh_file: str):
+        Args:
+            mesh_file (str): Filepath to load the mesh. OBJ format.
+        """
         super().__init__()
+
+        assert mesh_file.exists(), f"File missing: {mesh_file.absolute()}."
 
         self._mesh = kaolin.io.obj.import_mesh(mesh_file, with_materials=True)
         self._vertices = None
@@ -91,30 +75,16 @@ class DiffBBox(torch.nn.Module):
         # rotations as quaternion
         self._rotations = torch.tensor([0.0, 0.0, 0.0, 1.0], device="cuda", requires_grad=True)
         self._preprocess()
-        # TODO: add pose as quaternion. rotate verts
 
     def _preprocess(self):
         self._vertices = self._mesh.vertices.cuda().unsqueeze(0) * 75
         self._vertices.requires_grad = False
         self._faces = self._mesh.faces.cuda()
 
-    def _get_transform(self):
-        rot = None  # as 4x4
-        scale = None  # as 4x4
-        translation = None  # as 4x4
-        return torch.matmul(translation, torch.matmul(rot, scale))
-
     @property
     def vertices(self):
-        # rotate, scale in xyz, translate
-        # rot = euler_to_matrix33(self._rot_pitch, self._rot_roll, self._rot_yaw)
         rot = quaternion_to_matrix33(self._rotations)
-        # smat = scale_to_matrix44(self._scales)
-        # tmat = translation_to_matrix44(self._centers)
-        # vertices = F.pad(self._vertices, (0, 1), mode='constant', value=1.)
-
-        # return torch.matmul(torch.matmul(torch.matmul(vertices, rot2), smat), tmat)
-        # tmp = torch.cat([self._vertices, torch.ones(size=(1,8,1,)).cuda()], dim=-1)
+        # scale, rotate, and translate vertices by optimized transform coordinates
         return (torch.matmul(self._vertices, rot) * self._scales) + self._centers
 
     @property
@@ -138,7 +108,7 @@ def occupancy(mask):
 
 
 def project_to_2d(
-    bbox: DiffBBox,
+    bbox: DifferentiableBBox,
     batch_size: int,
     image_shape: Tuple[int, int],
     camera_transform,
@@ -154,7 +124,6 @@ def project_to_2d(
     nb_faces = bbox.faces.shape[0]
     face_attributes = [torch.ones((batch_size, nb_faces, 3, 1), device="cuda")]
 
-    # TODO: not sure what this is doing / expecting
     image_features, soft_mask, face_idx = kaolin.render.mesh.dibr_rasterization(
         image_shape[0],
         image_shape[1],
@@ -163,11 +132,11 @@ def project_to_2d(
         face_attributes,
         face_normals[:, :, -1],
     )
-    return soft_mask
+    return soft_mask  # only aligning images by 2d silhouette
 
 
 def draw_image(gt_mask, pred_mask):
-    # assumption: mask shape is [b,w,h]
+    # mask shape is [w,h]
     canvas = np.zeros((gt_mask.shape[0], gt_mask.shape[1], 3))
     canvas[..., 2] = pred_mask.cpu().detach().numpy()
     canvas[..., 1] = gt_mask.cpu().detach().numpy()
@@ -177,12 +146,11 @@ def draw_image(gt_mask, pred_mask):
 
 def show_renders(dataset: torch.Tensor):
     with torch.no_grad():
-        batch_size = len(dataset)
         images = []
-        for idx, data in enumerate(dataset):
-            gt_mask = data["semantic"].cuda()
-            camera_transform = data["metadata"]["cam_transform"].cuda()
-            camera_projection = data["metadata"]["cam_proj"].cuda()
+        for sample in dataset:
+            gt_mask = sample["semantic"].cuda()
+            camera_transform = sample["metadata"]["cam_transform"].cuda()
+            camera_projection = sample["metadata"]["cam_proj"].cuda()
 
             # project model mesh onto 2d image
             img_shape = (gt_mask.shape[0], gt_mask.shape[1])
@@ -196,25 +164,43 @@ def show_renders(dataset: torch.Tensor):
             image = draw_image(gt_mask.squeeze(), soft_mask)
             images.append(image)
 
-        # fig, ax = plt.subplots(1, batch_size, figsize=(4, 6))
-        # for i in range(batch_size):
-        #     ax[i].imshow(images[i])
-
         return images
 
 
-# set up model & optimization
-bbox = DiffBBox("../samples/bbox.obj")
+# hyperparameters
+batch_size_hyper = 2
+mask_weight_hyper = 1.0
+mask_occupancy_hyper = 0.05
+mask_overlap_hyper = 1.0
+lr = 5e-2
+scheduler_step_size = 5
+scheduler_gamma = 0.5
+num_epoch = 30
+
+# dataset parameters
+rendered_path = Path("../samples/rendered_clock/")
+mesh_path = Path("../samples/bbox.obj")
+
+# set up 2d image dataset including camera extrinsics:
+num_views = len(glob.glob(os.path.join(rendered_path, "*_rgb.png")))
+train_data = []
+for i in range(num_views):
+    data = kaolin.io.render.import_synthetic_view(rendered_path, i, rgb=True, semantic=True)
+    train_data.append(data)
+dataloader = DataLoader(train_data, batch_size=batch_size_hyper, shuffle=True, pin_memory=True)
+
+# set up model & optimization parameters
+bbox = DifferentiableBBox(mesh_path)  # simple 3d box mesh data
 optim = torch.optim.Adam(params=[bbox._centers, bbox._scales, bbox._rotations], lr=lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=scheduler_step_size, gamma=scheduler_gamma)
 
-
+# run training
 image_list = []
 for epoch in range(num_epoch):
     for idx, data in enumerate(dataloader):
         optim.zero_grad()
 
-        # get image mask and camera properties
+        # get image mask and camera extrinsics
         gt_mask = data["semantic"].cuda()
         camera_transform = data["metadata"]["cam_transform"].cuda()
         camera_projection = data["metadata"]["cam_proj"].cuda()
@@ -235,26 +221,30 @@ for epoch in range(num_epoch):
         mask_overlap = overlap(soft_mask, gt_mask.squeeze(-1))
 
         # loss = mask_loss * mask_weight
-        # penalize larger masks
-        # reward more overlap
+        #   mask_occupancy = penalize larger masks
+        #   mask_overlap = reward more overlap
         loss = mask_occupancy * mask_occupancy_hyper + mask_overlap * mask_overlap_hyper
 
         loss.backward()
         optim.step()
 
+        # view training progress
         if idx % 10 == 0:
-            test_batch_ids = [2, 5, 10]
+            test_batch_ids = [2, 5, 10]  # pick canonical test render views
             test_viz = [train_data[idx] for idx in test_batch_ids]
+            # only keep 1 in 10 renders to reduce animation processing time
             image_list.append(show_renders(test_viz))
 
             cv2.imshow("test", image_list[-1][0])
+            # minimal delay to ensure window updates
+            # disable to remove thread-switching cost.
             k = cv2.waitKey(1)
 
     scheduler.step()
-    print(f"{epoch} loss: {loss}")
+    print(f"loss on epoch {epoch:<}: {loss}")
 
 
-# generate final plot
+# generate final animation
 num_subplots = len(test_batch_ids)
 f, ax = plt.subplots(ncols=num_subplots)
 ims = []
