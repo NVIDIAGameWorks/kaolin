578e0d746d0744f678e85fe2ce45d4b5d523fb38
Frank Shen <frshen@nvidia.com>
2022-03-29 03:11:37 -0400

    add tetmesh subdivision
    Signed-off-by: Frank Shen <frshen@nvidia.com>

--- tests/python/kaolin/ops/mesh/test_tetmesh.py
+++ tests/python/kaolin/ops/mesh/test_tetmesh.py
@@ -45,3 +45,99 @@ class TestTetMeshOps:
                                  [79.9999, -149.9999, 10.0000],
                                  [-99.9999, 159.9998, -10.0000]]]])
         torch.allclose(tetmesh.inverse_vertices_offset(tetrahedrons), oracle)
+
+
+@pytest.mark.parametrize('device', ['cpu', 'cuda'])
+class TestSubdivideTetmesh:
+
+    @pytest.fixture(autouse=True)
+    def vertices_single_tet(self, device):
+        return torch.tensor([[[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]], dtype=torch.float, device=device)
+
+    @pytest.fixture(autouse=True)
+    def faces_single_tet(self, device):
+        return torch.tensor([[0, 1, 2, 3]], dtype=torch.long, device=device)
+
+    @pytest.fixture(autouse=True)
+    def expected_vertices_single_tet(self, device):
+        return torch.tensor([[[0.0000, 0.0000, 0.0000],
+                              [1.0000, 0.0000, 0.0000],
+                              [0.0000, 1.0000, 0.0000],
+                              [0.0000, 0.0000, 1.0000],
+                              [0.5000, 0.0000, 0.0000],
+                              [0.0000, 0.5000, 0.0000],
+                              [0.0000, 0.0000, 0.5000],
+                              [0.5000, 0.5000, 0.0000],
+                              [0.5000, 0.0000, 0.5000],
+                              [0.0000, 0.5000, 0.5000]]], dtype=torch.float, device=device)
+
+    @pytest.fixture(autouse=True)
+    def expected_faces_single_tet(self, device):
+        return torch.tensor([[0, 4, 5, 6],
+                             [1, 7, 4, 8],
+                             [2, 5, 7, 9],
+                             [3, 6, 9, 8],
+                             [4, 5, 6, 8],
+                             [4, 5, 8, 7],
+                             [9, 5, 8, 6],
+                             [9, 5, 7, 8]], dtype=torch.long, device=device)
+
+    @pytest.fixture(autouse=True)
+    def faces_two_tets(self, device):
+        return torch.tensor([[0, 1, 2, 3], [0, 1, 2, 3]], dtype=torch.long, device=device)
+
+    @pytest.fixture(autouse=True)
+    def expected_faces_two_tets(self, device):
+        return torch.tensor([[0, 4, 5, 6],
+                            [0, 4, 5, 6],
+                            [1, 7, 4, 8],
+                            [1, 7, 4, 8],
+                            [2, 5, 7, 9],
+                            [2, 5, 7, 9],
+                            [3, 6, 9, 8],
+                            [3, 6, 9, 8],
+                            [4, 5, 6, 8],
+                            [4, 5, 6, 8],
+                            [4, 5, 8, 7],
+                            [4, 5, 8, 7],
+                            [9, 5, 8, 6],
+                            [9, 5, 8, 6],
+                            [9, 5, 7, 8],
+                            [9, 5, 7, 8]], dtype=torch.long, device=device)
+
+    @pytest.fixture(autouse=True)
+    def features_single_tet(self, device):
+        return torch.tensor([[[-1, 2], [-1, 4], [0.5, -2], [0.5, -3]]], dtype=torch.float, device=device)
+
+    @pytest.fixture(autouse=True)
+    def expected_features_single_tet(self, device):
+        return torch.tensor([[[-1.0000, 2.0000],
+                              [-1.0000, 4.0000],
+                              [0.5000, -2.0000],
+                              [0.5000, -3.0000],
+                              [-1.0000, 3.0000],
+                              [-0.2500, 0.0000],
+                              [-0.2500, -0.5000],
+                              [-0.2500, 1.0000],
+                              [-0.2500, 0.5000],
+                              [0.5000, -2.5000]]], dtype=torch.float, device=device)
+
+    def test_subdivide_tetmesh_no_features(self, vertices_single_tet, faces_single_tet, expected_vertices_single_tet, expected_faces_single_tet):
+        new_vertices, new_faces = tetmesh.subdivide_tetmesh(vertices_single_tet, faces_single_tet)
+        assert torch.equal(new_vertices, expected_vertices_single_tet)
+        assert torch.equal(new_faces, expected_faces_single_tet)
+
+    def test_subdivide_tetmesh_no_features(self, vertices_single_tet, faces_single_tet, expected_vertices_single_tet, expected_faces_single_tet, features_single_tet, expected_features_single_tet):
+        new_vertices, new_faces, new_features = tetmesh.subdivide_tetmesh(
+            vertices_single_tet, faces_single_tet, features_single_tet)
+        assert torch.equal(new_vertices, expected_vertices_single_tet)
+        assert torch.equal(new_faces, expected_faces_single_tet)
+        assert torch.equal(new_features, expected_features_single_tet)
+
+    def test_subdivide_tetmesh_shared_verts(self, vertices_single_tet, faces_two_tets, expected_vertices_single_tet, expected_faces_two_tets, features_single_tet, expected_features_single_tet):
+        # check if redundant vertices are generated
+        new_vertices, new_faces, new_features = tetmesh.subdivide_tetmesh(
+            vertices_single_tet, faces_two_tets, features_single_tet)
+        assert torch.equal(new_vertices, expected_vertices_single_tet)
+        assert torch.equal(new_faces, expected_faces_two_tets)
+        assert torch.equal(new_features, expected_features_single_tet)
