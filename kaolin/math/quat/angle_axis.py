Fix documentation for quaternions (#810)
* start support for documentation for math

Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

* finish fixes

Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

---------

Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

kaolin/math/quat/angle_axis.py
@@ -20,8 +20,12 @@ from torch import Tensor
 
 from .quaternion import quat_from_rot33, quat_imaginary, quat_unit_positive, quat_real
 
-### CONVERSIONS ###
+__all__ = [
+    'angle_axis_from_quat',
+    'angle_axis_from_rot33'
+]
 
+### CONVERSIONS ###
 
 @torch.jit.script
 def angle_axis_from_quat(quat: Tensor) -> Tuple[Tensor, Tensor]:

kaolin/math/quat/euclidean.py
@@ -21,8 +21,16 @@ from torch import Tensor
 from .matrix44 import rot44_from_quat
 from .rotation33 import is_rot33_valid, rot33_inverse
 
-### CONVERSIONS ###
+__all__ = [
+    'euclidean_identity',
+    'euclidean_from_rotation_translation',
+    'euclidean_rotation_matrix',
+    'euclidean_translation_vector',
+    'is_euclidean_valid',
+    'euclidean_inverse'
+]
 
+### CONVERSIONS ###
 
 @torch.jit.script
 def euclidean_identity(batch_size: int, device: torch.device = "cuda") -> Tensor: