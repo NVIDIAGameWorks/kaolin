916135b9d8fed4c8410deac094da9213ab3e57cc
Clement Fuji Tsang <cfujitsang@nvidia.com>
2025-03-21 03:15:55 +0000

    add doc to cpp file
    Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

--- kaolin/csrc/ops/mesh/triangle_hash.h
+++ kaolin/csrc/ops/mesh/triangle_hash.h
@@ -3,11 +3,16 @@
 namespace kaolin {
 
 struct TriangleHash : torch::CustomClassHolder {
+  // This class is a 2D grid listing the triangle IDs
+  // where each pixel is overlapping with the triangle AABB
+  // A bit like tile-rendering
   std::vector<std::vector<int>> _spatial_hash { };
   int _resolution;
 
 public:
   TriangleHash(at::Tensor triangles, int resolution);
+
+  // check on the 2d grid the matching triangle ID for each 2d points
   std::vector<at::Tensor> query(at::Tensor points);
 
 private:
