2f13d1147e8f76ebd9fb26286892d2b90df8207c
operel <operel@nvidia.com>
2025-05-19 02:08:12 +0300

    polyscope-kaolin camera conversions
    MR fixes
    
    Signed-off-by: operel <operel@nvidia.com>
    
    fix docstrings 2
    
    Signed-off-by: operel <operel@nvidia.com>

--- docs/kaolin_ext.py
+++ docs/kaolin_ext.py
@@ -101,6 +101,7 @@ def run_apidoc(_):
             "kaolin/render/camera/legacy.py",
             "kaolin/render/camera/raygen.py",
             "kaolin/render/camera/gsplats.py",
+            "kaolin/render/camera/polyscope.py",
             "kaolin/non_commercial/flexicubes/",
             "kaolin/non_commercial/flexicubes/flexicubes.py",
             "kaolin/non_commercial/flexicubes/tables.py"
