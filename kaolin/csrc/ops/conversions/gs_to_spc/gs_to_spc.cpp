06409c4570520aced6d722c0125be97d144148a1
Clement Fuji Tsang <cfujitsang@nvidia.com>
2025-07-07 16:23:47 -0400

    add guard if no cuda, add some check on integrate_gs_cuda
    Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

--- kaolin/csrc/ops/conversions/gs_to_spc/gs_to_spc.cpp
+++ kaolin/csrc/ops/conversions/gs_to_spc/gs_to_spc.cpp
@@ -72,9 +72,6 @@ gs_to_spc_cuda(
   at::checkSize(__func__, means3D_arg, {num_gaussians, 3});
   at::checkSize(__func__, scales_arg, {num_gaussians, 3});
   at::checkSize(__func__, rotations_arg, {num_gaussians, 4});
-  at::checkScalarType(__func__, means3D_arg, at::kFloat);
-  at::checkScalarType(__func__, scales_arg, at::kFloat);
-  at::checkScalarType(__func__, rotations_arg, at::kFloat);
 
   return gs_to_spc_cuda_impl(means3D, scales, rotations, iso, tol, level);
 
@@ -93,7 +90,46 @@ at::Tensor integrate_gs_cuda(
   const uint32_t    level,
   const uint32_t    step) {
 
+#ifdef WITH_CUDA
+  at::TensorArg points_arg{points, "points", 0};
+  at::TensorArg gaus_id_arg{gaus_id, "gaus_id", 1};
+  at::TensorArg means3D_arg{means3D, "means3D", 2};
+  at::TensorArg cov3Dinv_arg{cov3Dinv, "cov3Dinv", 3};
+  at::TensorArg opacities_arg{opacities, "opacities", 4};
+
+  at::checkAllSameGPU(__func__, {
+      points_arg,
+      gaus_id_arg,
+      means3D_arg,
+      cov3Dinv_arg,
+      opacities_arg});
+  at::checkAllContiguous(__func__, {
+      points_arg,
+      gaus_id_arg,
+      means3D_arg,
+      cov3Dinv_arg,
+      opacities_arg});
+
+  at::checkScalarType(__func__, points_arg, at::kShort);
+  at::checkScalarType(__func__, gaus_id_arg, at::kLong);
+  at::checkScalarType(__func__, means3D_arg, at::kFloat);
+  at::checkAllSameType(__func__, {
+      means3D_arg,
+      cov3Dinv_arg,
+      opacities_arg});
+
+  const int num_intersection = points.size(0);
+  const int num_gaussians = means3D.size(0);
+  at::checkSize(__func__, points_arg, {num_intersection, 3});
+  at::checkSize(__func__, gaus_id_arg, {num_intersection});
+  at::checkSize(__func__, means3D_arg, {num_gaussians, 3});
+  at::checkSize(__func__, cov3Dinv_arg, {num_gaussians, 6});
+  at::checkNumel(__func__, opacities_arg, num_gaussians);
+
   return integrate_gs_cuda_impl(points, gaus_id, means3D, cov3Dinv, opacities, level, step);
+#else
+  KAOLIN_NO_CUDA_ERROR(__func__);
+#endif
 }
 
 
