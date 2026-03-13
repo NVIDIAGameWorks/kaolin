f9cea56a8ec45bb906ee297ed963a257ab710a70
Clement Fuji Tsang <cfujitsang@nvidia.com>
2026-03-13 00:00:06 -0400

    exclude kaolin/io/usd/gaussians.py
    Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

--- docs/kaolin_ext.py
+++ docs/kaolin_ext.py
@@ -37,6 +37,7 @@ def run_apidoc(_):
             "kaolin/io/usd/materials.py",
             "kaolin/io/usd/voxelgrid.py",
             "kaolin/io/usd/pointcloud.py",
+            "kaolin/io/usd/gaussians.py",
             "kaolin/math/quat/angle_axis.py",
             "kaolin/math/quat/euclidean.py",
             "kaolin/math/quat/matrix44.py",
